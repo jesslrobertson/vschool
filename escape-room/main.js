2c1dc52b6f8df814a62fb737012ef72c381d3829
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-02-09 10:03:00 -0700

    Did a few new Java Script exercises, and finished escape-room

--- escape-room/main.js
+++ escape-room/main.js
@@ -2,19 +2,38 @@ const readline = require("readline-sync");
 
 const actions = ["Look through the book", "Look in the box", "Search the hole", "Open the door"]
 
-console.log("You wake up in an unfamiliar place...")
-const name = readline.question("What is your name? ");
-console.log('Hi ' + name + ', it seems you are locked in this room. Take a look around...');
-index = readline.keyInSelect(actions, "What would you like to do?")
-console.log(actions[index]);
 let result;
-if (actions[index] === "Look in the box") {
-    result = ("\nhmm...just some old toys");
-} else if (actions[index] === "Look through the book") {
-    result = ("\nYou found a key!");
-} else if (actions[index] === "Open the door") {
+let hasKey = false;
+let isAlive = true;
+let gotOut = false;
+
+console.log("\nYou wake up in an unfamiliar place...")
+const name = readline.question("\nWhat is your name? ");
+console.log('\nHi ' + name + ', it seems you are locked in this room. Take a look around...');
+
+while (gotOut === false && isAlive === true) {
+    
+    index = readline.keyInSelect(actions, "\nWhat would you like to do?", {cancel: false})
+    console.log(actions[index]);
+    
+    if (actions[index] === "Look in the box") {
+        result = ("\nhmm...just some old toys");
+    } else if (actions[index] === "Look through the book") {
+        result = ("\nYou found a key!");
+        hasKey = true;
+        
+    } else if (hasKey === false && actions[index] === "Open the door") {
     result = ("\nThe door is locked.");
-} else if (actions[index] === "Search the hole") {
-    result = ("You died x_x")
-}
-console.log(result)
+    } 
+    else if (hasKey === true && actions[index] === "Open the door") {
+        result = ("\nYou Escaped!");
+        gotOut = true;
+    }
+    else if (actions[index] === "Search the hole") {
+        result = ("\nYou died x_x");
+        isAlive = false;
+    }
+    console.log(result);
+}        
+
+
---
+++ query-selector-practice/play.js
@@ -0,0 +1,10 @@
+var result = document.querySelectorAll("ol#favorite-things > li"); {
+    result[2].textContent = "Brown paper packages tied up with string.";
+}
+
+document.body.style.backgroundColor = "aquamarine";
+
+var header = document.getElementById("header")
+console.log(Object.keys(header.style))
+
+header.style.color = "green"
