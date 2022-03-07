f1b450b4c2c7691ba0417685cf4d8f38cb2f9ea2
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-03-07 12:31:12 -0700

    Completed travel form exercise

---
+++ travel-form/main.js
@@ -0,0 +1,19 @@
+const form = document['travelForm'];
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    let age = form.age.value;
+    const gender = form.gender.value;
+    const destination =  form.destination.value;
+    let dietaryRestrictions = [];
+    for (i = 0; i < form.diet.length; i++){
+        if(form.diet[i].checked){
+        dietaryRestrictions.push(form.diet[i].value);
+        }
+    }
+    alert(`First Name: ${firstName} \n Last Name: ${lastName}\nAge: ${age}\nGender: ${gender}\nDestination: ${destination}\nDiet: ${dietaryRestrictions}`);
+    form.reset();
+})
