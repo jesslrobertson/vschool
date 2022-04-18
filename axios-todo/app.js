b17da848449445fe80d65cb61c068cf98236562c
Jess Robertson <jesslaurenrobertson@gmail.com>
2022-04-18 12:42:03 -0600

    Todo list is fully functional! There is one error with editing image urls I'm not sure how to correct yet

--- axios-todo/app.js
+++ axios-todo/app.js
@@ -34,6 +34,40 @@ function displayList(data) {
 }
 
 function createListItem(item){
+    //create and append each item
+    const title = document.createElement('li');
+    title.textContent = item.title;
+    document.getElementById('todoList').appendChild(title);
+    //create and add title editor
+    const editTitle = document.createElement('input');
+    editTitle.type = "text";
+    editTitle.classList.add('editInput', 'title', 'input');
+    editTitle.value = item.title;
+    editTitle.classList.add('title', 'editInput');
+    editTitle.style.display = 'none';
+    document.getElementById('todoList').appendChild(editTitle);
+    //strikethrough finished items
+    if (item.completed === true) {
+        title.style.textDecoration = "line-through";
+    }
+    //add images if present
+    const todoImage = document.createElement('img');
+    const editImage = document.createElement('input');
+    if (item.hasOwnProperty('imgUrl') === true){
+        todoImage.src = item.imgUrl;
+        todoImage.style.maxWidth = "150px";
+        todoImage.style.maxHeight = "200px";
+        todoImage.style.display = "block";
+        todoImage.classList.add = "todo-image";
+        document.getElementById('todoList').appendChild(todoImage);
+        //add image source editor
+        editImage.type = 'text';
+        editImage.classList.add('editInput', 'image', 'input');
+        editImage.value = item.imgUrl;
+        editImage.style.display = 'none';
+        editImage.placeholder = "Image URL";
+        document.getElementById('todoList').appendChild(editImage);
+    }
     //add a checkbox to each item
     const checkbox = document.createElement('input');
     checkbox.type = "checkbox";
@@ -41,28 +75,29 @@ function createListItem(item){
     checkbox.checked = item.completed;
     checkbox.addEventListener("change", () => crossItOff(item._id, checkbox.checked));
     document.getElementById('todoList').appendChild(checkbox);
-    //create and append each item
-    const newLi = document.createElement('li');
-    const todoImage = document.createElement('img');
-    newLi.textContent = item.title;
-    //strikethrough finished items
-    if (item.completed === true) {
-        newLi.style.textDecoration = "line-through";
-    }
-    //add images if present
-    document.getElementById('todoList').appendChild(newLi);
-    if (item.hasOwnProperty('imgUrl') === true){
-        todoImage.src = item.imgUrl;
-        todoImage.style.maxWidth = "100px";
-        todoImage.style.maxHeight = "150px";
-        todoImage.style.display = "block";
-        todoImage.classList.add = "todo-image";
-        document.getElementById('todoList').appendChild(todoImage);
-    }
-    //add a divider after each item
-    // const divider = document.createElement('div');
-    // divider.classList.add = "divider";
-    // document.getElementById('todoList').appendChild(divider);
+    //add delete button for each item
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = "Delete";
+    deleteButton.classList.add("delete");
+    deleteButton.addEventListener("click", () => deleteItem(item._id));
+    document.getElementById('todoList').appendChild(deleteButton);
+    //add edit button
+    const editButton = document.createElement('button');
+    editButton.innerText = "Edit";
+    editButton.classList.add('edit');
+    editButton.addEventListener("click", () => editItem(item, todoImage, saveButton, editTitle, editButton, title, editImage));
+    document.getElementById('todoList').appendChild(editButton);
+    //add save button
+    const saveButton = document.createElement('button');
+    saveButton.innerText = "Save";
+    saveButton.classList.add('save');
+    saveButton.addEventListener("click", () => saveItem(item, editTitle, title, editImage, todoImage, editButton, saveButton));
+    saveButton.style.display = "none";
+    document.getElementById('todoList').appendChild(saveButton);
+    // add a divider after each item
+    const divider = document.createElement('div');
+    divider.classList.add = "divider";
+    document.getElementById('todoList').appendChild(divider);
 }
 function clearList(){
     const el = document.getElementById('todoList')
@@ -78,6 +113,39 @@ function crossItOff(itemId, status){
     axios.put("https://api.vschool.io/jessr/todo/" + itemId, {completed: status})
     .then(() => getData())
     .catch(err => console.log(err));
+}
+
+function deleteItem(itemId){
+    console.log(itemId);
+    axios.delete("https://api.vschool.io/jessr/todo/" + itemId)
+    .then(() =>getData())
+    .catch(err => console.log(err));
+
+}
+
+function editItem (item, todoImage, saveButton, editTitle, editButton, title, editImage ){
+    saveButton.style.display = 'inline';
+    editTitle.style.display = 'block';
+    editButton.style.display = 'none';
+    title.style.display = 'none';
+    editTitle.value = item.title;
+    todoImage.style.display = 'none';
+    editImage.style.display = 'block';
+}
+
+function saveItem(item, editTitle, title, editImage, todoImage, editButton, saveButton){
+    newTitle = editTitle.value;
+    todoImage.src = editImage.value;
+    axios.put("https://api.vschool.io/jessr/todo/" + item._id, { title: newTitle,
+    imgUrl: todoImage.src })
+    .then(() => getData())
+    .catch(err => console.log(err));
+    title.style.display = 'block';
+    todoImage.style.display = 'block';
+    editButton.style.display = 'inline';
+    saveButton.style.display = 'none';
+    editButton.style.display = 'inline';
+    editImage.style.display = 'none';
 }
 
 getData();
